bfeed6f165fa6038e14be1695964e27fad2c75c3
Klara H <>
2021-06-19 22:09:52 +0200

    padding footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer style={{ marginTop: 160, fontSize: "0.5rem", }}>
-        <div style={{ marginLeft: 16 }}>
+      <footer style={{ padding: 16, fontSize: "0.5rem", }}>
+        <div>
           Dieses Tool wurde von xxx gebaut???
           {/* put some more Info about the Tool here? */}
         </div>
